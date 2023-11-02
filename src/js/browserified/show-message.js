853e596fea8095e98fb7bff29f2684001fff0f97
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-11-02 21:38:19 +0900

    test focus and blur event

--- src/js/browserified/show-message.js
+++ src/js/browserified/show-message.js
@@ -1656,17 +1656,25 @@ module.exports = time_estimates;
 
 
 },{}],8:[function(require,module,exports){
-const checkPassword = require("zxcvbn")
+const checkPassword = require("zxcvbn");
 
 
-const passwordElements = document.querySelectorAll("input[type=password]")
+const passwordElements = document.querySelectorAll("input[type=password]");
 
-const amount = passwordElements.length
+const amount = passwordElements.length;
 if (amount > 0) {
-    pass = prompt("ユーザー名を入力してください", "")
-    const result = checkPassword(pass)
-    alert(result.guesses + "\n" + result.feedback.warning + "\n" + result.feedback.suggestions)
+    // pass = prompt("ユーザー名を入力してください", "")
+    // const result = checkPassword(pass)
+    // alert(result.guesses + "\n" + result.feedback.warning + "\n" + result.feedback.suggestions)
 
+    passwordElements.forEach((value, key, parent) => {
+        value.addEventListener("focus", (event) => {
+            event.target.value = "password";
+        });
+        value.addEventListener("blur", (event) => {
+            event.target.value = "";
+        });
+    });
 }
 
 },{"zxcvbn":4}]},{},[8]);
--- src/js/unbrowserified/show-message.js
+++ src/js/unbrowserified/show-message.js
@@ -1,12 +1,20 @@
-const checkPassword = require("zxcvbn")
+const checkPassword = require("zxcvbn");
 
 
-const passwordElements = document.querySelectorAll("input[type=password]")
+const passwordElements = document.querySelectorAll("input[type=password]");
 
-const amount = passwordElements.length
+const amount = passwordElements.length;
 if (amount > 0) {
-    pass = prompt("ユーザー名を入力してください", "")
-    const result = checkPassword(pass)
-    alert(result.guesses + "\n" + result.feedback.warning + "\n" + result.feedback.suggestions)
+    // pass = prompt("ユーザー名を入力してください", "")
+    // const result = checkPassword(pass)
+    // alert(result.guesses + "\n" + result.feedback.warning + "\n" + result.feedback.suggestions)
 
+    passwordElements.forEach((value, key, parent) => {
+        value.addEventListener("focus", (event) => {
+            event.target.value = "password";
+        });
+        value.addEventListener("blur", (event) => {
+            event.target.value = "";
+        });
+    });
 }
