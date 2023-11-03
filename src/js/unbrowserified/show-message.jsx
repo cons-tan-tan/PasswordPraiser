cb89754a8904a79ff557b6c9099226e38a4a2850
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-11-03 21:16:33 +0900

    add button for test

--- src/js/unbrowserified/show-message.jsx
+++ src/js/unbrowserified/show-message.jsx
@@ -1,39 +1,49 @@
 const checkPassword = require("zxcvbn");
 const React = require("react");
-const ReactDOM = require("react-dom/client")
-const Button = require("@mui/material/Button")
+const ReactDOM = require("react-dom/client");
+const Button= require("@mui/material").Button;
 
+
+const App = () => {
+    return <div id="password-praiser-ui"
+        style={{
+            position: 'absolute',
+            width: '0%',
+            left: '0px',
+            top: '0px',
+            zIndex: 2147483550,
+        }}>
+        <Button variant="contained" color="primary" onClick={() => {
+            let result = checkPassword("asasasfgg");
+            alert(result.score);
+        }}>test</Button>
+    </div>
+};
 
 const passwordElements = document.querySelectorAll("input[type=password]");
 const amount = passwordElements.length;
 if (amount > 0) {
-    // pass = prompt("ユーザー名を入力してください", "")
-    // const result = checkPassword(pass)
-    // alert(result.guesses + "\n" + result.feedback.warning + "\n" + result.feedback.suggestions)
+
+    let praiser = document.createElement("div");
+    praiser.id = "password-praiser";
+    document.body.appendChild(praiser);
+    const root = ReactDOM.createRoot(praiser);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 
     passwordElements.forEach((value, key, parent) => {
         value.addEventListener("focus", (event) => {
             event.target.value = "password";
+            const clientRect = event.target.getBoundingClientRect();
+            let praiser_ui = document.getElementById("password-praiser-ui");
+            praiser_ui.style.left = clientRect.left + "px";
+            praiser_ui.style.top = clientRect.top + "px";
         });
         value.addEventListener("blur", (event) => {
             event.target.value = "";
         });
     });
 }
-
-const App = () => {
-    return <div>
-        Hello
-    </div>
-}
-
-
-let praiser = document.createElement("div");
-praiser.id = "password-praiser";
-document.body.appendChild(praiser);
-const root = ReactDOM.createRoot(praiser);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
